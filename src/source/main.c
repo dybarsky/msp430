2b59fe28765c385f171007961c7921753572b944
Maxim Dybarskiy <maxim.dybarskyy@gmail.com>
2014-07-04 02:14:47 +0300

    Use true-c code style

--- src/source/main.c
+++ src/source/main.c
@@ -1,8 +1,8 @@
 #include <msp430g2553.h>
 
-#define RED BIT0
-#define GREEN BIT6
-#define BUTTON BIT3
+#define RED 	BIT0
+#define GREEN 	BIT6
+#define BUTTON 	BIT3
 
 void wait() {
 	unsigned int count;
@@ -12,31 +12,32 @@ void wait() {
 void blink(int led) {
 	unsigned int count;
 	for (count = 0; count < 6; count++) {
-		P1OUT ^= led;
+		P1OUT ^= led;							// переключаем состояние пина
 		wait();
 	}
 }
 
 int main(void) {
-    WDTCTL = WDTPW + WDTHOLD; // отключаем сторожевой таймер
-    
-    // используем откалиброваную частоту для 1MHZ
-    BCSCTL1 = CALBC1_1MHZ;
+    WDTCTL = WDTPW + WDTHOLD; 					// отключаем сторожевой таймер
+    				
+    BCSCTL1 = CALBC1_1MHZ;						// используем откалиброваную частоту для 1MHZ
 	DCOCTL = CALDCO_1MHZ;
     
-    P1OUT = 0;
-    P1DIR = GREEN + RED;
+    P1OUT = 0;									// обнуляем регистр
+    P1DIR = GREEN + RED;						// устанавливаем на выход пины 0 и 6
     
-    // Для LaunchPad версии 1.5 используем резистор
-    P1REN |= BUTTON; //разрешаем подтяжку 
-    P1OUT |= BUTTON; //подтяжка вывода P1.3 вверх
+												// Для LaunchPad версии 1.5 используем резистор
+    P1REN |= BUTTON; 							//разрешаем подтяжку 
+    P1OUT |= BUTTON; 							//подтяжка вывода P1.3 вверх
         
     while (1) {
-		blink(GREEN);
-		while ((P1IN & BUTTON) == BUTTON);
-		blink(RED);
-		// используем откалиброваную частоту для 8MHZ
-		BCSCTL1 = CALBC1_8MHZ;
+		blink(GREEN);							// мигаем зелёным
+		
+		while ((P1IN & BUTTON) == BUTTON);		// ждём пока не нажмётся кнопка
+		
+		blink(RED);								// мигаем красным
+		
+		BCSCTL1 = CALBC1_8MHZ;					// используем откалиброваную частоту для 8MHZ
 		DCOCTL = CALDCO_8MHZ;
     }
 }
