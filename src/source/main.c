0adfb99d8f0685b436e250e859120df4e0890278
Maxim Dybarskiy <maxim.dybarskyy@gmail.com>
2014-07-13 23:57:03 +0300

    Time from frequency

--- src/source/main.c
+++ src/source/main.c
@@ -3,7 +3,6 @@
 #define OUT_PORT	P2OUT
 #define OUT_DIR		P2DIR
 #define GREEN_LED	BIT3
-#define BUTTON 		BIT3
 
 int count;
 
@@ -18,12 +17,8 @@ void _configure();
 //~
 
 int main(void) {
-
     _configure();								// первоначальная настройка контроллера
-
-    while (1) {
-		_blink(GREEN_LED);							// мигаем диодом
-    }
+    while(1);
 }
 
 void _configure() {
@@ -35,18 +30,9 @@ void _configure() {
     OUT_PORT = 0;								// обнуляем регистр
     OUT_DIR = GREEN_LED;						// устанавливаем на выход пин 3
 
-												// Для LaunchPad версии 1.5 используем резистор
-    P1REN |= BUTTON; 							// разрешаем подтяжку
-    P1OUT |= BUTTON; 							// подтяжка вывода P1.3 вверх
-
-	TACCR0 = 62500 - 1;							// период 62,500 циклов, от 0 до 62,499.
+	TACCR0 = 62500 - 1;							// за 0.5 сек
 	TACCTL0 = CCIE;  							// Разрешаем прерывание таймера по достижению значения CCR0.
-	TACTL = TASSEL_2 + ID_2 + MC_1 + TACLR;		// sub-mainClock + делитель частоты 8 + прямой счёт + инициализация
-
-	P1IES |= BUTTON;  							// прерывание для 3-го порта по переходу из 1 в 0
-	P1IFG &= ~BUTTON; 							// обнуляем флаг прерывания для 3-го бита
-												// для предотвращения немедленного срабатывания прерывания
-	P1IE |= BUTTON;   							// Разрешаем прерывания для порта 3
+	TACTL = TASSEL_2 + ID_3 + MC_1 + TACLR;		// sub-mainClock + делитель честоты 8 + прямой счёт + инициализация
 
 	_BIS_SR(GIE);								// разрешаем прерывания
 }
@@ -55,41 +41,13 @@ void _configure() {
 
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void _on_timer(void) {
-    // Сброс флага прерывания таймера происходит автоматически, ручное обнуление не нужно
-    if (++count > 30) {
-		BCSCTL1 = CALBC1_8MHZ;				// используем откалиброваную частоту для 8MHZ
-		DCOCTL = CALDCO_8MHZ;
-	}
-}
-
-#pragma vector = PORT1_VECTOR
-__interrupt void _on_button_pressed(void) {
-    switch(P1IFG & BIT3) {
-        case BIT3:
-            P1IFG &= ~BUTTON;  					// обнуляем флаг прерывания для P1.3
-            BCSCTL1 = CALBC1_1MHZ;				// используем откалиброваную частоту для 8MHZ
-			DCOCTL = CALDCO_1MHZ;
-			count = 0;
-            return;
-        default:
-            P1IFG = 0;   						// Возможно в этом нет необходимости, но обнуляем
-												// флаги всех прерываний в P1, на всякий случай.
-												// Хотя лучше было бы добавить обработчик ошибки.
-            return;
-    }
-}
-
-//~
-
-void _wait() {
-	unsigned int count;
-	for (count = 0; count < 60000; count++);
-}
-
-void _blink(int led) {
-	unsigned int count;
-	for (count = 0; count < 6; count++) {
-		OUT_PORT ^= led;							// переключаем состояние пина
-		_wait();
+	if (++count == 3) count = 0;
+	switch (count) {
+		case 0 :
+			OUT_PORT = GREEN_LED;
+			break;
+		case 1:
+			OUT_PORT = ~GREEN_LED;
+			break;
 	}
 }
