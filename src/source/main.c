6c88183c45c29b827cb663487770b901e708bf83
Maxim Dybarskiy <maxim.dybarskyy@gmail.com>
2014-07-15 01:09:53 +0300

    PWM with led

--- src/source/main.c
+++ src/source/main.c
@@ -4,13 +4,18 @@
 #define OUT_DIR		P2DIR
 #define GREEN_LED	BIT3
 
+#define SC_MACRO	100
+#define SC_SECOND	5000
+#define SC_COUNT	49 // 50 - 1
+#define SIZE		5
+
 int count;
+int index;
+int voltage;
+
+int arr[SIZE] = {10, 30, 50, 70, 90};
 
 //~
-
-void _wait();
-
-void _blink(int led_bit);
 
 void _configure();
 
@@ -18,6 +23,7 @@ void _configure();
 
 int main(void) {
     _configure();								// первоначальная настройка контроллера
+    OUT_PORT = GREEN_LED;						// включаем диод
     _BIS_SR(LPM0_bits);							// переходим в энергоэффективный режим
 }
 
@@ -30,9 +36,9 @@ void _configure() {
     OUT_PORT = 0;								// обнуляем регистр
     OUT_DIR = GREEN_LED;						// устанавливаем на выход пин 3
 
-	TACCR0 = 62500 - 1;							// за 0.5 сек
+	TACCR0 = SC_COUNT;							// за 1/(50*100) сек
 	TACCTL0 = CCIE;  							// Разрешаем прерывание таймера по достижению значения CCR0.
-	TACTL = TASSEL_2 + ID_3 + MC_1 + TACLR;		// sub-mainClock + делитель честоты 8 + прямой счёт + инициализация
+	TACTL = TASSEL_2 + ID_2 + MC_1 + TACLR;		// sub-mainClock + делитель частоты 4 + прямой счёт + инициализация
 
 	_BIS_SR(GIE);								// разрешаем прерывания
 }
@@ -40,14 +46,19 @@ void _configure() {
 //~
 
 #pragma vector = TIMER0_A0_VECTOR
-__interrupt void _on_timer(void) {
-	if (++count == 3) count = 0;				//	используются значения от 0 до 3
-	switch (count) {
-		case 0 :
-			OUT_PORT = GREEN_LED;				// на 0 выключаем; t=0.5s
-			break;
-		case 1:
-			OUT_PORT = ~GREEN_LED;				// на 1 включаем (на 2 уже включено); t=1s
-			break;
+__interrupt void _on_timer(void) {				// макро-такт - каждое 100-е прерывание (за 1/50 секунды)
+	int relativeCount = ++count % SC_MACRO;		// относительное значение счётчика в рамках макро-такта
+	if (relativeCount == 0) {					// начало макро-такта
+		OUT_PORT = GREEN_LED;					// включаем диод
+	}
+	if (relativeCount == voltage) {				// % достигнут
+		OUT_PORT = ~GREEN_LED;					// выключаем диод
+	}
+	if (count == SC_SECOND) {					// 1 секунда
+		count = 0;
+		if (++index == SIZE) index = 0;			// циклический инкремент
+		voltage = arr[index];					// следующая частота
 	}
 }
+
+
